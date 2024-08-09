3fd37ce6b2e2efc7e5e8125adfad4b70c87da508
NicatDev <nicat254memmedov@gmail.com>
2024-08-09 17:45:09 +0400

    interests

--- feedapp/views.py
+++ feedapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView
+from feedapp.serializers import CategorySerializer
+from feedapp.models import Category
 # Create your views here.
+
+class CategoryListView(ListAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
